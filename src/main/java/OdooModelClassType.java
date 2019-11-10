6e9b410e69efcfe07e988dc51276e8c493fd13fc
Trinh Anh Ngoc <ngoc.trinh@tvtmarine.com>
2019-11-10 18:33:47 +0700

    Super

--- src/main/java/OdooModelClassType.java
+++ src/main/java/OdooModelClassType.java
@@ -1,16 +1,20 @@
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDirectory;
-import com.jetbrains.python.psi.PyClass;
+import com.intellij.psi.PsiElement;
+import com.jetbrains.python.psi.*;
+import com.jetbrains.python.psi.resolve.PyResolveContext;
+import com.jetbrains.python.psi.resolve.RatedResolveResult;
 import com.jetbrains.python.psi.types.PyClassLikeType;
 import com.jetbrains.python.psi.types.PyClassTypeImpl;
 import com.jetbrains.python.psi.types.TypeEvalContext;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class OdooModelClassType extends PyClassTypeImpl {
     private OdooModelInfo myOdooModelInfo;
+    private List<PyClassLikeType> cachedAncestorTypes = null;
 
     public OdooModelClassType(@NotNull PyClass source, boolean isDefinition) {
         super(source, isDefinition);
@@ -28,6 +32,22 @@ public class OdooModelClassType extends PyClassTypeImpl {
         supers.forEach(pyClass -> {
             result.add(new OdooModelClassType(pyClass, myIsDefinition));
         });
+        return result;
+    }
+
+    @NotNull
+    @Override
+    public List<PyClassLikeType> getAncestorTypes(@NotNull TypeEvalContext context) {
+        if (cachedAncestorTypes != null) {
+            return cachedAncestorTypes;
+        }
+        List<PyClassLikeType> result = new LinkedList<>();
+        getSuperClasses().forEach(pyClass -> {
+            OdooModelClassType type = new OdooModelClassType(pyClass, myIsDefinition);
+            result.add(type);
+            result.addAll(type.getAncestorTypes(context));
+        });
+        cachedAncestorTypes = result;
         return result;
     }
 
@@ -61,5 +81,41 @@ public class OdooModelClassType extends PyClassTypeImpl {
             }
             result.addAll(pyClasses);
         }
+    }
+
+    @Nullable
+    public PsiElement findMember(@NotNull String name, PyResolveContext resolveContext, boolean inherit) {
+        TypeEvalContext context = resolveContext.getTypeEvalContext();
+        PyTargetExpression attExpr = myClass.findClassAttribute(name, false, context);
+        if (attExpr != null) {
+            return attExpr;
+        }
+        PyFunction funcExpr = myClass.findMethodByName(name, false, context);
+        if (funcExpr != null) {
+            return funcExpr;
+        }
+        if (inherit) {
+            for (PyClassLikeType classType : getAncestorTypes(context)) {
+                if (classType instanceof OdooModelClassType) {
+                    PsiElement member = ((OdooModelClassType) classType).findMember(name, resolveContext, false);
+                    if (member != null) {
+                        return member;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public List<? extends RatedResolveResult> resolveMember(@NotNull String name, @Nullable PyExpression location, @NotNull AccessDirection direction, @NotNull PyResolveContext resolveContext, boolean inherited) {
+        if (myOdooModelInfo != null) {
+            PsiElement element = findMember(name, resolveContext, inherited);
+            if (element != null) {
+                return Collections.singletonList(new RatedResolveResult(RatedResolveResult.RATE_NORMAL, element));
+            }
+        }
+        return super.resolveMember(name, location, direction, resolveContext, inherited);
     }
 }
