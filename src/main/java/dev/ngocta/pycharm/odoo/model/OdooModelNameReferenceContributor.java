37048e93b56bb310bb6133f498d9407bf827e748
Trinh Anh Ngoc <ngoc.trinh@tvtmarine.com>
2019-11-29 23:12:53 +0700

    Goodnight

--- src/main/java/dev/ngocta/pycharm/odoo/model/OdooModelNameReferenceContributor.java
+++ src/main/java/dev/ngocta/pycharm/odoo/model/OdooModelNameReferenceContributor.java
@@ -6,26 +6,27 @@ import com.intellij.psi.PsiReferenceContributor;
 import com.intellij.psi.PsiReferenceRegistrar;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.python.psi.PyArgumentList;
-import com.jetbrains.python.psi.PyCallExpression;
+import com.jetbrains.python.psi.PyReferenceExpression;
 import com.jetbrains.python.psi.PyStringLiteralExpression;
+import dev.ngocta.pycharm.odoo.OdooNames;
 import org.jetbrains.annotations.NotNull;
 
 import static com.intellij.patterns.PlatformPatterns.psiElement;
 
 public class OdooModelNameReferenceContributor extends PsiReferenceContributor {
-    public static final PsiElementPattern.Capture<PyStringLiteralExpression> ODOO_NAME_PATTERN =
+    public static final PsiElementPattern.Capture<PyStringLiteralExpression> COMODEL_NAME_PATTERN =
             psiElement(PyStringLiteralExpression.class).withParent(
-                    psiElement(PyArgumentList.class).withParent(
-                            psiElement(PyCallExpression.class).with(new PatternCondition<PyCallExpression>("abcd") {
+                    psiElement(PyArgumentList.class).afterSibling(
+                            psiElement(PyReferenceExpression.class).with(new PatternCondition<PyReferenceExpression>("comodel") {
                                 @Override
-                                public boolean accepts(@NotNull PyCallExpression pyCallExpression, ProcessingContext context) {
-                                    return false;
+                                public boolean accepts(@NotNull PyReferenceExpression pyReferenceExpression, ProcessingContext context) {
+                                    String name = pyReferenceExpression.getName();
+                                    return OdooNames.MANY2ONE.equals(name) || OdooNames.ONE2MANY.equals(name) || OdooNames.MANY2MANY.equals(name);
                                 }
-                            })
-                    ));
+                            })));
 
     @Override
     public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
-        registrar.registerReferenceProvider(ODOO_NAME_PATTERN, new OdooModelNameReferenceProvider());
+        registrar.registerReferenceProvider(COMODEL_NAME_PATTERN, new OdooModelNameReferenceProvider());
     }
 }
