b356f05bb78a3dfb6c82a5459fe13c25dc3a9a44
Trinh Anh Ngoc <ngoc.trinh@tvtmarine.com>
2019-12-26 22:09:56 +0700

    Fix super() resolving

--- src/main/java/dev/ngocta/pycharm/odoo/model/OdooSuperMembersProvider.java
+++ src/main/java/dev/ngocta/pycharm/odoo/model/OdooSuperMembersProvider.java
@@ -3,6 +3,7 @@ package dev.ngocta.pycharm.odoo.model;
 import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNamedElement;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.Processor;
 import com.jetbrains.python.PyNames;
 import com.jetbrains.python.codeInsight.PyCustomMember;
@@ -12,61 +13,74 @@ import com.jetbrains.python.psi.PyExpression;
 import com.jetbrains.python.psi.resolve.PyResolveContext;
 import com.jetbrains.python.psi.types.PyClassMembersProvider;
 import com.jetbrains.python.psi.types.PyClassType;
+import com.jetbrains.python.psi.types.PyType;
 import com.jetbrains.python.psi.types.TypeEvalContext;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
 
 public class OdooSuperMembersProvider implements PyClassMembersProvider {
     @NotNull
     @Override
     public Collection<PyCustomMember> getMembers(PyClassType type, PsiElement location, @NotNull TypeEvalContext context) {
-        List<PyCustomMember> result = new LinkedList<>();
-        visitSuperMembers(type, context, member -> {
-            if (member.getName() != null) {
-                result.add(new PyCustomMember(member.getName(), member));
-            }
-            return true;
-        });
-        return result;
+        return Collections.emptyList();
     }
 
     @Nullable
     @Override
     public PsiElement resolveMember(@NotNull PyClassType type, @NotNull String name, @Nullable PsiElement location, @NotNull PyResolveContext resolveContext) {
         Ref<PsiElement> ref = new Ref<>();
+        TypeEvalContext context = resolveContext.getTypeEvalContext();
         if (location instanceof PyCallExpression) {
             PyExpression callee = ((PyCallExpression) location).getCallee();
             if (callee != null && PyNames.SUPER.equals(callee.getName())) {
-                visitSuperMembers(type, resolveContext.getTypeEvalContext(), member -> {
-                    if (name.equals(member.getName())) {
-                        ref.set(member);
-                        return false;
+                PyClass instanceClass = null;
+                PyClass superClass = null;
+                PyExpression[] args = ((PyCallExpression) location).getArguments();
+                if (args.length == 2) {
+                    PyType superType = context.getType(args[0]);
+                    if (superType instanceof PyClassType) {
+                        superClass = ((PyClassType) superType).getPyClass();
                     }
-                    return true;
-                });
+                    PyType instanceType = context.getType(args[1]);
+                    if (instanceType instanceof PyClassType) {
+                        instanceClass = ((PyClassType) instanceType).getPyClass();
+                    }
+                } else if (args.length == 0) {
+                    PyClass cls = PsiTreeUtil.getParentOfType(location, PyClass.class);
+                    if (cls != null) {
+                        OdooModelClass modelClass = OdooModelUtils.getContainingOdooModelClass(cls);
+                        if (modelClass != null) {
+                            instanceClass = modelClass;
+                            superClass = cls;
+                        }
+                    }
+                }
+                if (instanceClass != null && superClass != null) {
+                    visitSuperMembers(instanceClass, superClass, context, member -> {
+                        if (name.equals(member.getName())) {
+                            ref.set(member);
+                            return false;
+                        }
+                        return true;
+                    });
+                }
             }
         }
         return ref.get();
     }
 
-    private void visitSuperMembers(@NotNull PyClassType type, @NotNull TypeEvalContext context, @NotNull Processor<PsiNamedElement> processor) {
-        PyClass cls = type.getPyClass();
-        if (cls instanceof OdooModelClass) {
-            return;
-        }
-        OdooModelClass modelCls = OdooModelUtils.getContainingOdooModelClass(cls);
-        if (modelCls != null) {
-            List<PyClass> ancestors = modelCls.getAncestorClasses(context);
-            int idx = ancestors.indexOf(cls);
-            if (idx >= 0) {
-                for (PyClass parent : ancestors.subList(idx, ancestors.size())) {
-                    if (!parent.visitMethods(processor::process, false, context)) {
-                        break;
-                    }
+    private void visitSuperMembers(@NotNull PyClass instanceClass, @NotNull PyClass superClass,
+                                   @NotNull TypeEvalContext context, @NotNull Processor<PsiNamedElement> processor) {
+        List<PyClass> ancestors = instanceClass.getAncestorClasses(context);
+        int idx = ancestors.indexOf(superClass);
+        if (idx >= 0) {
+            for (PyClass parent : ancestors.subList(idx + 1, ancestors.size())) {
+                if (!parent.visitMethods(processor::process, false, context)) {
+                    break;
                 }
             }
         }
