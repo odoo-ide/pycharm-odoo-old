86712841d3f404611b6b48eee3a27e0c16260a19
Trinh Anh Ngoc <ngoc.trinh@tvtmarine.com>
2020-03-29 17:56:00 +0700

    Go to symbol: respect search scope

--- src/main/java/dev/ngocta/pycharm/odoo/data/OdooGotoExternalIdContributor.java
+++ src/main/java/dev/ngocta/pycharm/odoo/data/OdooGotoExternalIdContributor.java
@@ -25,7 +25,7 @@ public class OdooGotoExternalIdContributor implements ChooseByNameContributorEx 
     @Override
     public void processElementsWithName(@NotNull String name, @NotNull Processor<NavigationItem> processor, @NotNull FindSymbolParameters parameters) {
         Project project = parameters.getProject();
-        Collection<OdooRecord> records = OdooExternalIdIndex.findRecordsById(name, project);
+        Collection<OdooRecord> records = OdooExternalIdIndex.findRecordsById(name, parameters.getSearchScope());
         records.forEach(record -> {
             record.getNavigationItems(project).forEach(processor::process);
         });
