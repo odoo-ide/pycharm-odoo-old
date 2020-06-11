9923eec89498ff20fe189ab4d656f60f1d953203
Trinh Anh Ngoc <atw1990@gmail.com>
2020-06-11 22:11:51 +0700

    Fix function param type with default value is False

---
+++ src/main/java/dev/ngocta/pycharm/odoo/OdooFunctionParamBooleanTypeProvider.java
@@ -0,0 +1,28 @@
+package dev.ngocta.pycharm.odoo;
+
+import com.intellij.openapi.util.Ref;
+import com.jetbrains.python.psi.PyBoolLiteralExpression;
+import com.jetbrains.python.psi.PyFunction;
+import com.jetbrains.python.psi.PyNamedParameter;
+import com.jetbrains.python.psi.types.PyType;
+import com.jetbrains.python.psi.types.PyTypeProviderBase;
+import com.jetbrains.python.psi.types.TypeEvalContext;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class OdooFunctionParamBooleanTypeProvider extends PyTypeProviderBase {
+    @Nullable
+    @Override
+    public Ref<PyType> getParameterType(@NotNull PyNamedParameter param,
+                                        @NotNull PyFunction func,
+                                        @NotNull TypeEvalContext context) {
+        if (param.getDefaultValue() instanceof PyBoolLiteralExpression) {
+            // In Odoo, the default value for a recordset param is usually False,
+            // that causes PyCharm infers its type is bool.
+            // To avoid errors such as "Unresolved attribute reference xyz for class bool",
+            // its type should be Any instead.
+            return Ref.create();
+        }
+        return super.getParameterType(param, func, context);
+    }
+}
