fc43ec6d1c8180d80945a7e48400e648234c0d7e
Trinh Anh Ngoc <atw1990@gmail.com>
2020-06-16 18:48:23 +0700

    Make import quick fix work with odoo translate _

---
+++ src/main/java/dev/ngocta/pycharm/odoo/OdooImportCandidateProvider.java
@@ -0,0 +1,29 @@
+package dev.ngocta.pycharm.odoo;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.util.QualifiedName;
+import com.jetbrains.python.codeInsight.imports.AutoImportQuickFix;
+import com.jetbrains.python.codeInsight.imports.PyImportCandidateProvider;
+import com.jetbrains.python.psi.resolve.PyResolveImportUtil;
+import dev.ngocta.pycharm.odoo.module.OdooModuleUtils;
+
+public class OdooImportCandidateProvider implements PyImportCandidateProvider {
+    @Override
+    public void addImportCandidates(PsiReference psiReference,
+                                    String s,
+                                    AutoImportQuickFix autoImportQuickFix) {
+        PsiElement element = psiReference.getElement();
+        if (!OdooModuleUtils.isInOdooModule(element)) {
+            return;
+        }
+        if ("_".equals(s)) {
+            QualifiedName qualifiedName = QualifiedName.fromDottedString("odoo.tools.translate._");
+            PsiElement importElement = PyResolveImportUtil.resolveTopLevelMember(qualifiedName, PyResolveImportUtil.fromFoothold(element));
+            if (importElement == null) {
+                return;
+            }
+            autoImportQuickFix.addImport(importElement, importElement.getContainingFile(), QualifiedName.fromComponents("odoo"), null);
+        }
+    }
+}
