bbcf8e146407f6b270ce2191130b6003a1c67896
Trinh Anh Ngoc <atw1990@gmail.com>
2020-08-04 22:16:53 +0700

    Fix javascript resolve scope
    Fix #14

--- src/main/java/dev/ngocta/pycharm/odoo/javascript/OdooJSElementScopeProvider.java
+++ src/main/java/dev/ngocta/pycharm/odoo/javascript/OdooJSElementScopeProvider.java
@@ -1,21 +1,40 @@
 package dev.ngocta.pycharm.odoo.javascript;
 
-import com.intellij.lang.javascript.psi.resolve.JSElementResolveScopeProvider;
+import com.intellij.lang.javascript.psi.resolve.JavaScriptResolveScopeProvider;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.util.CachedValueProvider;
+import com.intellij.psi.util.CachedValuesManager;
+import com.intellij.psi.util.PsiModificationTracker;
 import dev.ngocta.pycharm.odoo.python.module.OdooModule;
 import dev.ngocta.pycharm.odoo.python.module.OdooModuleUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class OdooJSElementScopeProvider implements JSElementResolveScopeProvider {
+import java.util.List;
+
+public class OdooJSElementScopeProvider extends JavaScriptResolveScopeProvider {
     @Nullable
     @Override
     public GlobalSearchScope getElementResolveScope(@NotNull PsiElement psiElement) {
-        OdooModule odooModule = OdooModuleUtils.getContainingOdooModule(psiElement);
-        if (odooModule != null) {
-            return odooModule.getOdooModuleWithDependenciesScope();
+        PsiFile file = psiElement.getContainingFile();
+        OdooModule odooModule = OdooModuleUtils.getContainingOdooModule(file);
+        if (odooModule == null) {
+            return null;
         }
-        return null;
+        GlobalSearchScope modulesScope = odooModule.getOdooModuleWithDependenciesScope();
+        GlobalSearchScope librariesScope = getPredefinedLibraryScope(file.getProject());
+        return modulesScope.union(librariesScope);
+    }
+
+    private GlobalSearchScope getPredefinedLibraryScope(@NotNull Project project) {
+        return CachedValuesManager.getManager(project).getCachedValue(project, () -> {
+            List<VirtualFile> predefinedLibraryFiles = this.getPredefinedLibraryFiles(project);
+            GlobalSearchScope librariesScope = GlobalSearchScope.filesScope(project, predefinedLibraryFiles);
+            return CachedValueProvider.Result.create(librariesScope, PsiModificationTracker.MODIFICATION_COUNT);
+        });
     }
 }
