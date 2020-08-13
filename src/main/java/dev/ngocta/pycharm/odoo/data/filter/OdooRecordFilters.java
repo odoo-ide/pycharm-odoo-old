c9398600ff149eb2f3059b561176e24604561a4c
Trinh Anh Ngoc <atw1990@gmail.com>
2020-08-13 13:06:47 +0700

    Fix menu item parent completion

--- src/main/java/dev/ngocta/pycharm/odoo/data/filter/OdooRecordFilters.java
+++ src/main/java/dev/ngocta/pycharm/odoo/data/filter/OdooRecordFilters.java
@@ -6,7 +6,7 @@ import dev.ngocta.pycharm.odoo.data.OdooRecordViewInfo;
 
 public class OdooRecordFilters {
     public static final OdooRecordFilter RES_GROUPS = new OdooRecordModelFilter(OdooNames.RES_GROUPS);
-    public static final OdooRecordFilter IR_UI_MENU = new OdooRecordModelFilter(OdooNames.IR_UI_VIEW);
+    public static final OdooRecordFilter IR_UI_MENU = new OdooRecordModelFilter(OdooNames.IR_UI_MENU);
     public static final OdooRecordFilter ACTION_MODELS = new OdooRecordModelFilter(OdooNames.ACTION_MODELS);
     public static final OdooRecordFilter QWEB = record -> {
         OdooRecordViewInfo viewInfo = ObjectUtils.tryCast(record.getExtraInfo(), OdooRecordViewInfo.class);
