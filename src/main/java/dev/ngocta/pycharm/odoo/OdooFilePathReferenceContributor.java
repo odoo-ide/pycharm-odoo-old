2d0d9446297493d495cde4be43b2f399c7a951b3
Trinh Anh Ngoc <atw1990@gmail.com>
2020-08-15 16:58:43 +0700

    Recognize t-thumbnail references

--- src/main/java/dev/ngocta/pycharm/odoo/OdooFilePathReferenceContributor.java
+++ src/main/java/dev/ngocta/pycharm/odoo/OdooFilePathReferenceContributor.java
@@ -1,13 +1,14 @@
-package dev.ngocta.pycharm.odoo.xml;
+package dev.ngocta.pycharm.odoo;
 
 import com.intellij.patterns.XmlAttributeValuePattern;
 import com.intellij.patterns.XmlPatterns;
 import com.intellij.psi.PsiReferenceContributor;
+import com.intellij.psi.PsiReferenceProvider;
 import com.intellij.psi.PsiReferenceRegistrar;
-import dev.ngocta.pycharm.odoo.OdooFilePathReferenceProvider;
+import dev.ngocta.pycharm.odoo.xml.OdooXmlUtils;
 import org.jetbrains.annotations.NotNull;
 
-public class OdooXmlElementReferenceContributor extends PsiReferenceContributor {
+public class OdooFilePathReferenceContributor extends PsiReferenceContributor {
     public static final XmlAttributeValuePattern SCRIPT_SRC_PATTERN =
             XmlPatterns.xmlAttributeValue().withParent(
                     XmlPatterns.xmlAttribute("src").withParent(
@@ -20,9 +21,17 @@ public class OdooXmlElementReferenceContributor extends PsiReferenceContributor 
                             XmlPatterns.xmlTag().withLocalName("link"))).with(
                     OdooXmlUtils.ODOO_XML_DATA_ELEMENT_PATTERN_CONDITION);
 
+    public static final XmlAttributeValuePattern T_THUMBNAIL_PATTERN =
+            XmlPatterns.xmlAttributeValue().withParent(
+                    XmlPatterns.xmlAttribute("t-thumbnail").withParent(
+                            XmlPatterns.xmlTag().withLocalName("t"))).with(
+                    OdooXmlUtils.ODOO_XML_DATA_ELEMENT_PATTERN_CONDITION);
+
     @Override
     public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
-        registrar.registerReferenceProvider(SCRIPT_SRC_PATTERN, new OdooFilePathReferenceProvider());
-        registrar.registerReferenceProvider(LINK_HREF_PATTERN, new OdooFilePathReferenceProvider());
+        PsiReferenceProvider provider = new OdooFilePathReferenceProvider();
+        registrar.registerReferenceProvider(SCRIPT_SRC_PATTERN, provider);
+        registrar.registerReferenceProvider(LINK_HREF_PATTERN, provider);
+        registrar.registerReferenceProvider(T_THUMBNAIL_PATTERN, provider);
     }
 }
