9f2a00f20d3b2cbf2e1da81d4ad4fdf60e8b8205
Trinh Anh Ngoc <atw1990@gmail.com>
2020-09-29 20:59:43 +0700

    Gen doc for record id

--- src/main/java/dev/ngocta/pycharm/odoo/data/OdooRecordDocumentProvider.java
+++ src/main/java/dev/ngocta/pycharm/odoo/data/OdooRecordDocumentProvider.java
@@ -2,7 +2,12 @@ package dev.ngocta.pycharm.odoo.data;
 
 import com.intellij.lang.documentation.DocumentationMarkup;
 import com.intellij.lang.documentation.DocumentationProvider;
+import com.intellij.pom.PomTarget;
+import com.intellij.pom.PomTargetPsiElement;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.util.xml.DomTarget;
+import dev.ngocta.pycharm.odoo.xml.dom.OdooDomRecordLike;
 import org.jetbrains.annotations.Nullable;
 
 public class OdooRecordDocumentProvider implements DocumentationProvider {
@@ -10,8 +15,21 @@ public class OdooRecordDocumentProvider implements DocumentationProvider {
     @Override
     public String generateDoc(PsiElement element,
                               @Nullable PsiElement originalElement) {
-        if (element instanceof OdooRecordElement) {
-            OdooRecordElement recordElement = (OdooRecordElement) element;
+        OdooRecordElement recordElement = null;
+        if (element instanceof PomTargetPsiElement) {
+            PomTarget target = ((PomTargetPsiElement) element).getTarget();
+            if (target instanceof DomTarget && ((DomTarget) target).getDomElement() instanceof OdooDomRecordLike) {
+                OdooDomRecordLike domRecordLike = (OdooDomRecordLike) ((DomTarget) target).getDomElement();
+                OdooRecord record = domRecordLike.getRecord();
+                XmlTag tag = domRecordLike.getXmlTag();
+                if (record != null && tag != null) {
+                    recordElement = new OdooRecordElement(record, tag);
+                }
+            }
+        } else if (element instanceof OdooRecordElement) {
+            recordElement = (OdooRecordElement) element;
+        }
+        if (recordElement != null) {
             return DocumentationMarkup.DEFINITION_START +
                     recordElement.getPresentableText() +
                     DocumentationMarkup.GRAYED_START +
