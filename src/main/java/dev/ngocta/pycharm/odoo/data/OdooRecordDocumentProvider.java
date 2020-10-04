a5d10c0d83d732be919c98b0ef34cb2321d056f9
Trinh Anh Ngoc <atw1990@gmail.com>
2020-10-04 22:15:44 +0700

    Improve record element description

--- src/main/java/dev/ngocta/pycharm/odoo/data/OdooRecordDocumentProvider.java
+++ src/main/java/dev/ngocta/pycharm/odoo/data/OdooRecordDocumentProvider.java
@@ -2,12 +2,7 @@ package dev.ngocta.pycharm.odoo.data;
 
 import com.intellij.lang.documentation.DocumentationMarkup;
 import com.intellij.lang.documentation.DocumentationProvider;
-import com.intellij.pom.PomTarget;
-import com.intellij.pom.PomTargetPsiElement;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.xml.XmlTag;
-import com.intellij.util.xml.DomTarget;
-import dev.ngocta.pycharm.odoo.xml.dom.OdooDomRecordLike;
 import org.jetbrains.annotations.Nullable;
 
 public class OdooRecordDocumentProvider implements DocumentationProvider {
@@ -15,35 +10,11 @@ public class OdooRecordDocumentProvider implements DocumentationProvider {
     @Override
     public String generateDoc(PsiElement element,
                               @Nullable PsiElement originalElement) {
-        OdooRecordElement recordElement = null;
-        if (element instanceof PomTargetPsiElement) {
-            PomTarget target = ((PomTargetPsiElement) element).getTarget();
-            if (target instanceof DomTarget && ((DomTarget) target).getDomElement() instanceof OdooDomRecordLike) {
-                OdooDomRecordLike domRecordLike = (OdooDomRecordLike) ((DomTarget) target).getDomElement();
-                OdooRecord record = domRecordLike.getRecord();
-                XmlTag tag = domRecordLike.getXmlTag();
-                if (record != null && tag != null) {
-                    recordElement = new OdooRecordElement(record, tag);
-                }
-            }
-        } else if (element instanceof OdooRecordElement) {
-            recordElement = (OdooRecordElement) element;
-        }
-        if (recordElement != null) {
+        if (element instanceof OdooRecordElement) {
             return DocumentationMarkup.DEFINITION_START +
-                    recordElement.getPresentableText() +
-                    DocumentationMarkup.GRAYED_START +
-                    " (" + recordElement.getLocationString() + ")" +
-                    DocumentationMarkup.GRAYED_END +
+                    ((OdooRecordElement) element).getPresentableText() +
                     DocumentationMarkup.DEFINITION_END;
         }
         return null;
-    }
-
-    @Override
-    @Nullable
-    public String getQuickNavigateInfo(PsiElement element,
-                                       PsiElement originalElement) {
-        return generateDoc(element, originalElement);
     }
 }
--- src/main/java/dev/ngocta/pycharm/odoo/data/OdooRecordElementDescriptionProvider.java
+++ src/main/java/dev/ngocta/pycharm/odoo/data/OdooRecordElementDescriptionProvider.java
@@ -1,9 +1,17 @@
 package dev.ngocta.pycharm.odoo.data;
 
+import com.intellij.pom.PomTarget;
+import com.intellij.pom.PomTargetPsiElement;
 import com.intellij.psi.ElementDescriptionLocation;
 import com.intellij.psi.ElementDescriptionProvider;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.xml.XmlTag;
 import com.intellij.usageView.UsageViewShortNameLocation;
+import com.intellij.usageView.UsageViewTypeLocation;
+import com.intellij.util.xml.DomElement;
+import com.intellij.util.xml.DomTarget;
+import dev.ngocta.pycharm.odoo.xml.OdooXmlUtils;
+import dev.ngocta.pycharm.odoo.xml.dom.OdooDomRecordLike;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -12,8 +20,25 @@ public class OdooRecordElementDescriptionProvider implements ElementDescriptionP
     @Nullable
     public String getElementDescription(@NotNull PsiElement element,
                                         @NotNull ElementDescriptionLocation location) {
-        if (element instanceof OdooRecordElement && location instanceof UsageViewShortNameLocation) {
-            return ((OdooRecordElement) element).getRecord().getUnqualifiedId();
+        if (element instanceof OdooRecordElement) {
+            if (location instanceof UsageViewShortNameLocation) {
+                return ((OdooRecordElement) element).getRecord().getUnqualifiedId();
+            } else if (location instanceof UsageViewTypeLocation) {
+                if (element.getOriginalElement() instanceof XmlTag) {
+                    return ((XmlTag) element.getOriginalElement()).getName();
+                }
+                return ((OdooRecordElement) element).getRecord().getModel();
+            }
+        } else if (element instanceof XmlTag && OdooXmlUtils.isOdooXmlDataElement(element)) {
+            return ((XmlTag) element).getName();
+        } else if (element instanceof PomTargetPsiElement) {
+            PomTarget target = ((PomTargetPsiElement) element).getTarget();
+            if (target instanceof DomTarget) {
+                DomElement domElement = ((DomTarget) target).getDomElement();
+                if (domElement instanceof OdooDomRecordLike) {
+                    return domElement.getXmlElementName();
+                }
+            }
         }
         return null;
     }
