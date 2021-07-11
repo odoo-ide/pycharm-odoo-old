61ae984126e4c710f485ad14069e948b70432cb1
Trinh Anh Ngoc <atw1990@gmail.com>
2021-07-11 18:30:27 +0700

    Respect default xml formatter
    Fix #43

--- src/main/java/dev/ngocta/pycharm/odoo/xml/OdooXmlPolicy.java
+++ src/main/java/dev/ngocta/pycharm/odoo/xml/OdooXmlPolicy.java
@@ -2,9 +2,15 @@ package dev.ngocta.pycharm.odoo.xml;
 
 import com.intellij.formatting.FormattingDocumentModel;
 import com.intellij.formatting.WrapType;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.formatter.xml.XmlPolicy;
+import com.intellij.psi.xml.XmlElementType;
 import com.intellij.psi.xml.XmlTag;
+import com.intellij.psi.xml.XmlText;
+import com.intellij.util.xml.DomUtil;
+import dev.ngocta.pycharm.odoo.xml.dom.OdooDomViewElement;
 
 public class OdooXmlPolicy extends XmlPolicy {
     public OdooXmlPolicy(CodeStyleSettings settings,
@@ -14,7 +20,19 @@ public class OdooXmlPolicy extends XmlPolicy {
 
     @Override
     public WrapType getWrappingTypeForTagBegin(XmlTag tag) {
-        return WrapType.NONE;
+        if (DomUtil.findDomElement(tag, OdooDomViewElement.class) != null) {
+            PsiElement prevSibling = tag.getPrevSibling();
+            if (prevSibling instanceof XmlText) {
+                PsiElement[] children = prevSibling.getChildren();
+                if (children.length == 1 && children[0] instanceof PsiWhiteSpace) {
+                    prevSibling = prevSibling.getPrevSibling();
+                }
+            }
+            if (prevSibling.getNode().getElementType() != XmlElementType.XML_COMMENT) {
+                return WrapType.NONE;
+            }
+        }
+        return super.getWrappingTypeForTagBegin(tag);
     }
 
     @Override
